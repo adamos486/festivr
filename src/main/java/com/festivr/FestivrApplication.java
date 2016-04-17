bdfb83f1d4fd82f49b2682ebd3aac99317841f0d
adamos486 <adamos486@gmail.com>
2016-04-16 23:08:35 -0700

    Adding the internet permission to the Manfiest. Adding a static Singleton application instance for easy context access.

--- src/main/java/com/festivr/FestivrApplication.java
+++ src/main/java/com/festivr/FestivrApplication.java
@@ -3,5 +3,14 @@ package com.festivr;
 import android.app.Application;
 
 public class FestivrApplication extends Application {
+  private static FestivrApplication instance;
 
+  public static FestivrApplication getSingleton() {
+    return instance;
+  }
+
+  @Override public void onCreate() {
+    super.onCreate();
+    instance = this;
+  }
 }
