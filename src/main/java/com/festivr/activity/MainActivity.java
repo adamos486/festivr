a516933da149a66966c24b3f10407ee5d3935cfd
adamos486 <adamos486@gmail.com>
2016-04-17 23:05:49 -0700

    Cleanup and more config examples.

--- src/main/java/com/festivr/activity/MainActivity.java
+++ src/main/java/com/festivr/activity/MainActivity.java
@@ -3,13 +3,13 @@ package com.festivr.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import com.festivr.R;
 import com.festivr.adapter.ChooseExperienceAdapter;
+import com.festivr.cache.LruCache;
 import com.festivr.image.Configuration;
 import com.festivr.image.Images;
 import com.festivr.utils.Constants;
@@ -20,7 +20,6 @@ public class MainActivity extends Activity {
 
   private ListView artistOptions;
   private ChooseExperienceAdapter adapter;
-  private Handler handler = new Handler();
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -39,7 +38,7 @@ public class MainActivity extends Activity {
       @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ArrayList<String> imageUrls = new ArrayList<String>();
         String title = "";
-        switch(position) {
+        switch (position) {
           case ChooseExperienceAdapter.PORTER_ROBINSON_INDEX:
             imageUrls = Constants.getPorterRobinson();
             title = getString(R.string.porter_robinson);
@@ -64,55 +63,27 @@ public class MainActivity extends Activity {
             imageUrls = Constants.getHugeList();
             title = getString(R.string.everything);
             break;
-
         }
 
         Intent experienceIntent = new Intent(MainActivity.this, ExperienceImagesActivity.class);
         experienceIntent.putExtra("title", title);
         experienceIntent.putStringArrayListExtra("imageUrls", imageUrls);
         startActivity(experienceIntent);
-        overridePendingTransition(R.anim.activity_slide_right_in,
-            R.anim.activity_slide_left_out);
+        overridePendingTransition(R.anim.activity_slide_right_in, R.anim.activity_slide_left_out);
       }
     });
 
     Images.getSingleton().setup(new Configuration.Builder(this).build());
-  }
 
-  //private void runThroughImages() {
-  //  List<String> list = Constants.getHugeList();
-  //  for (int i = 0; i < list.size(); i++) {
-  //    final int index = i;
-  //    final String s = list.get(i);
-  //    handler.postDelayed(new Runnable() {
-  //      @Override public void run() {
-  //        Timber.d("Index: " + index + "Trying to load " + s);
-  //        Images.getSingleton().load(new UrlKeyCombo(s), imageView, new ImageLoadingListener() {
-  //          @Override public void loadingStarted(UrlKeyCombo combo, ImageViewWrapper view) {
-  //
-  //          }
-  //
-  //          @Override
-  //          public void loadingFailed(UrlKeyCombo combo, ImageViewWrapper view, String error) {
-  //
-  //          }
-  //
-  //          @Override public void loadingComplete(UrlKeyCombo combo, final ImageViewWrapper view,
-  //              final Bitmap bitmap) {
-  //            runOnUiThread(new Runnable() {
-  //              @Override public void run() {
-  //                Timber.d("setting " + index + " in view");
-  //                view.setImageBitmap(bitmap);
-  //              }
-  //            });
-  //          }
-  //
-  //          @Override public void loadingCancelled(UrlKeyCombo combo, ImageViewWrapper view) {
-  //
-  //          }
-  //        });
-  //      }
-  //    }, (50 * i));
-  //  }
-  //}
+    //ALTERNATIVE
+    //Images.getSingleton()
+    //    .setup(new Configuration.Builder(this).setMemoryCacheSize(52428800)
+    //        .setMemoryCachePool(25)
+    //        .setMemoryCacheMode(this, LruCache.MEDIUM_MEMORY)
+    //        .build());
+
+    //ANOTHER ALTERNATIVE
+    //Images.getSingleton()
+    //    .setup(new Configuration.Builder(this).setMemoryCache(new LruCache(52428800, 25)).build());
+  }
 }
